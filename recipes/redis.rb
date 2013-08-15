fe0b2931e6c42cd70ef5a291cd6c8aa3c97be0b0
Elad Meidar <elad@eizesus.com>
2013-08-15 17:18:45 +0300

    [FIX] adding the redis recipe a configuration file

--- recipes/redis.rb
+++ recipes/redis.rb
@@ -1,9 +1,26 @@
-gem 'redis'
+gem "hiredis", "~> 0.4.0"
+gem "redis", ">= 2.2.0", :require => ["redis", "redis/connection/hiredis"]
+
+
+say_wizard "Generating Redis Configuration..."
+create_file "config/redis.yml"
 
 say_wizard "Generating Redis initializer..."
-
+append_file "config/redis.yml" do <<-RUBY
+default:
+  host: localhost
+  port: 6379
+development:
+  db: 0
+test:
+  db: 1
+production:
+  db: 2
+  RUBY
+end
 initializer "redis.rb", <<-RUBY
-REDIS = Redis.new
+redis_conf = YAML::load(File.read(Rails.root.join("config", "redis.yml")))[Rails.env]
+REDIS = Redis.new(redis_conf)
 RUBY
 
 __END__
